3450232f2cb1cbc746fdeea919ea67012d61e68c
Phillip Nguyen <phillip.d.nguyen23@gmail.com>
2015-08-10 20:19:39 -0700

    js changes
    jquery edits

--- js/app.js
+++ js/app.js
@@ -1,23 +1,49 @@
 $(document).ready(function() {
+  function playHadouken() {
+    $('#hadouken-sound')[0].volume=0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+  }
+
   $('.ryu').mouseenter(function() {
     $('.ryu-still').hide();
     $('.ryu-ready').show();
   })
+
   .mouseleave(function() {
     $('.ryu-ready').hide();
     $('.ryu-still').show();
   })
+
   .mousedown(function() {
-    // play hadouken sound
+    playHadouken();
     $('.ryu-ready').hide();
     $('.ryu-throwing').show();
-    $('.hadouken').show();
-    // show hadouken and animate it to the right of the screen
+    $('.hadouken').finish().show().animate(
+      {'left': '1020px'},
+      500,
+      function() {
+        $(this).hide();
+        $(this).css('left', '520px');
+      });
   })
+
   .mouseup(function() {
     $('.ryu-throwing').hide();
     $('.ryu-ready').show();
     // ryu goes back to his ready position
+  })
+
+  $(document).keydown(function(e) {
+    if (e.which == 88) {
+      $(".ryu-cool").show();
+      $(".ryu-still").hide();
+    };
+  }).keyup(function(e) {
+    if (e.which == 88) {
+      $(".ryu-cool").hide();
+      $(".ryu-still").show();
+    }
   });
 
 });
